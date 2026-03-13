23804dc9d31c2493d1ffa558382d79f79dbb69ec
AarikGuy <110928268+AarikGuy@users.noreply.github.com>
2026-03-12 23:08:33 -0600

    Cleaning up resume widget

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,7 +1,7 @@
 function About(){
     return(
     <section id="about">
-        <summary><h2>About Me</h2></summary>
+        <h2>About Me</h2>
         <p>
             Hi! I’m Aarik Guy, a Software Accessibility Tester at QualityLogic in the Boise Idaho office. I specialize in 
             Ensuring clients websites/softwares are more accessible to a wider audience. I love solving problems, learning new tools, 
--- frontend/src/components/Resume.jsx
+++ frontend/src/components/Resume.jsx
@@ -5,10 +5,9 @@ function Resume(){
     <section id="resume">
     <h2>Resume</h2>
     <p>
-        This is my most up-to-date resume, and I’ll continue updating it as my career grows. I created it entirely in 
-        <a href="https://en.wikipedia.org/wiki/LaTeX" title="Learn more about LaTeX"><strong>LaTeX</strong></a> 
-        using <a href="https://www.overleaf.com" title="Visit Overleaf"><strong>Overleaf</strong></a> which makes it easy 
-        to generate clean, professional PDF documents that are perfect for technical writing.
+        This is my most up-to-date resume, and I’ll continue updating it as my career grows. I created it entirely   
+        in <a href="https://en.wikipedia.org/wiki/LaTeX" title="Learn more about LaTeX"><strong>LaTeX</strong></a> using <a href="https://www.overleaf.com" title="Visit Overleaf"><strong>Overleaf</strong></a> which
+        makes it easy to generate clean, professional PDF documents that are perfect for technical writing.
     </p>
 
     <iframe
