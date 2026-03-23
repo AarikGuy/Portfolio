1592ff805f135fce7fbb478184485949031c6c8a
AarikGuy <110928268+AarikGuy@users.noreply.github.com>
2026-03-22 21:21:44 -0600

    Updated navbar and additional resposive design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,7 @@
 function Footer(){
     return(
         <footer>
-            <section id="contact-info">
-                <ul>
-                    <li>Email: <a href="mailto:adgofficialuser@gmail.com">adgofficialuser@gmail.com</a></li>
-                    <li>LinkedIn: <a href="https://www.linkedin.com/in/aarik-guy/" target="_blank">linkedin.com/in/AarikGuy</a></li>
-                    <li>GitHub: <a href="https://github.com/AarikGuy">github.com/AarikGuy</a></li>
-                </ul>
-            </section>
-
-            <p>&copy; 2025 Aarik Guy. All rights reserved.</p>
+            <p>&copy; 2026 Aarik Guy. All rights reserved.</p>
         </footer>
     );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,28 @@
+import facebookLogo from "../assets/facebook.png";
+import githubLogo from "../assets/github.png";
+import instagramLogo from "../assets/instagram.png";
+import linkedinLogo from "../assets/linkedin.png";
+
+
 function Navbar() {
     return(
     <header>
         <nav>
-            <h1>Portfolio</h1>
-            <ul>
+            <ul className="nav-links">
                 <li><a href="#about">About</a></li>
                 <li><a href="#resume">Resume</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
+
+        <div className="contact-logos">
+            <ul>
+                <li><a href="https://www.facebook.com/aarik.guy/"><img src={facebookLogo}></img></a></li>
+                <li><a href="https://www.instagram.com/aarikguy/"><img src={instagramLogo}></img></a></li>
+                <li><a href="https://www.linkedin.com/in/aarik-guy/"><img src={linkedinLogo}></img></a></li>
+                <li><a href="https://github.com/AarikGuy"><img src={githubLogo}></img></a></li>
+            </ul>
+        </div>
         </nav>
     </header>
     );
