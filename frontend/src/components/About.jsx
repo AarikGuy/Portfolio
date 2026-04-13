f1ae36d6626a10efc0bc3991668dc719e2f79e8b
AarikGuy <110928268+AarikGuy@users.noreply.github.com>
2026-04-12 22:17:27 -0600

    Implementing accordion widget for faq

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,14 +1,54 @@
 function About(){
-    return(
+
+    const FAQs = [
+        {
+            question: "What are your career goals?",
+            answer: "I plan to get hired as a Software Engineer working in web development with a specialty in accessibility. I enjoy building websites and applications"
+        },
+        {
+            question: "",
+            answer: ""
+        }
+    ];
+
+    return (
     <section id="about">
-        <h2>About Me</h2>
-        <p>
-            Hi! I’m Aarik Guy, a Software Accessibility Tester at QualityLogic in the Boise Idaho office. I specialize in 
-            ensuring clients websites/softwares are more accessible to a wider audience. I love solving problems, learning new tools, 
-            and helping clients improve their user experience.
-        </p>
+      <h2>About Me</h2>
+
+      <div className="accordion my-accordion" id="accordionFlushExample">
+        {FAQs.map((faq, index) => {
+          const headingId = `flush-heading${index}`;
+          const collapseId = `flush-collapse${index}`;
+
+          return (
+            <div className="accordion-item" key={index}>
+              <h2 className="accordion-header" id={headingId}>
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#${collapseId}`}
+                  aria-expanded="false"
+                  aria-controls={collapseId}
+                >
+                  {faq.question}
+                </button>
+              </h2>
+
+              <div
+                id={collapseId}
+                className="accordion-collapse collapse"
+                aria-labelledby={headingId}
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div className="accordion-body">{faq.answer}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
-    );
+  );
 }
 
 export default About;
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -14,8 +14,9 @@ function Hero() {
           </h1>
 
           <p>
-            I’m currently a Software Accessibility Tester passionate about
-            building impactful digital experiences.
+            I’m currently a Software Accessibility Tester working at QualityLogic in the Boise Idaho office. 
+            I specialize in ensuring clients websites/softwares are more accessible to a wider audience. I love solving problems, learning new tools, 
+            and helping clients improve their user experience.
           </p>
 
           <a href="/projects" className="btn">
