e154be1a9524e1b96f54c4d59403344cf7fc2595
AarikGuy <110928268+AarikGuy@users.noreply.github.com>
2026-04-19 21:09:45 -0600

    fixed reflow issue with projects intro text

--- frontend/src/components/Resume.jsx
+++ frontend/src/components/Resume.jsx
@@ -8,8 +8,7 @@ function Resume(){
             <h2>Resume</h2>
             <p>
                 This is my most up-to-date resume, and I’ll continue updating it as my career grows. I created it entirely   
-                in <a href="https://en.wikipedia.org/wiki/LaTeX" title="Learn more about LaTeX"><strong>LaTeX</strong></a> using 
-                <a href="https://www.overleaf.com" title="Visit Overleaf"><strong>Overleaf</strong></a> which
+                in <a href="https://en.wikipedia.org/wiki/LaTeX" title="Learn more about LaTeX"><strong>LaTeX</strong></a> using <a href="https://www.overleaf.com" title="Visit Overleaf"><strong>Overleaf</strong></a> which 
                 makes it easy to generate clean, professional PDF documents that are perfect for technical writing. 
                 If you wish to have a copy of my resume, here's a download <a href={resumeDoc} download="Resume_AarikGuy.pdf" aria-label='resume download link'>link</a>.
             </p>
